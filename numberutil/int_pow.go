cf71a4d00dc3e90010d9a14d3724e629197872df
Jon Hall <jon@flowmo.co>
2017-03-04 18:56:48 +0000

    Add integer power function utility

---
+++ numberutil/int_pow.go
@@ -0,0 +1,15 @@
+package numberutil
+
+// Integer power: compute a**b using binary powering algorithm
+// See https://groups.google.com/forum/#!topic/golang-nuts/PnLnr4bc9Wo
+func Pow(a, b int) int {
+    p := 1
+    for b > 0 {
+        if b&1 != 0 {
+            p *= a
+        }
+        b >>= 1
+        a *= a
+    }
+    return p
+}
---
+++ numberutil/int_pow_test.go
@@ -0,0 +1,23 @@
+package numberutil
+
+import "testing"
+
+func TestPow(t *testing.T) {
+	cases := []struct {
+		num int
+		exponent int
+		want int
+	}{
+		{1, 1, 1},
+		{2, 4, 16},
+		{1883, 0, 1},
+		{5, 3, 125},
+	}
+
+	for _, c := range cases {
+		got := Pow(c.num, c.exponent)
+		if got != c.want {
+			t.Errorf("Pow(%d, %d) == %d, want %d", c.num, c.exponent, got, c.want)
+		}
+	}
+}
