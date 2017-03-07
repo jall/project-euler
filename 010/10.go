f4599b1817e862a32722a7ce450eb6a3bd99056b
Jon Hall <jon@flowmo.co>
2017-03-07 19:40:16 +0000

    Question 10

---
+++ 010/10.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jall/project-euler/numberutil"
+)
+
+func main() {
+	primes := numberutil.SieveOfEratosthenes(2e6)
+	fmt.Println(sum(primes))
+}
+
+func sum(numbers []int) int {
+    total := 0
+
+    for _, number := range numbers {
+        total += number
+    }
+
+    return total
+}
