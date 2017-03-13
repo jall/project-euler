eef0539d1daf34536c49649a3c9062953aab8c17
Jon Hall <jon@flowmo.co>
2017-03-13 20:12:08 +0000

    Question 20

---
+++ 020/20.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jall/project-euler/numberutil"
+	"math/big"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	bigNumber := numberutil.FactorialBigInt(big.NewInt(100))
+	sum := 0
+
+	for _, character := range strings.Split(bigNumber.String(), "") {
+		digit, _ := strconv.Atoi(character)
+		sum += digit
+	}
+
+	fmt.Println("Sum of 100!: ", sum)
+	fmt.Println("100!: ", bigNumber)
+}
