30de6a98b20f73287ecc8bf13bf360c173f1a375
Jon Hall <jon@flowmo.co>
2017-04-14 18:59:13 +0100

    Question 30

---
+++ 030/30.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jall/project-euler/numberutil"
+	"strconv"
+	"strings"
+)
+
+// https://en.wikipedia.org/wiki/Narcissistic_number
+//
+// Skip the single digit numbers because they 'aren't a sum'.
+//
+// Max is because 6 digits is the maximum length before the sum of digit powers stops growing fast enough to
+// reach the digit count required, and 6*(9^5) = 354294.
+//
+// The highest output for any individual digit is 9^5 = 59049, and a brief experiment shows that only numbers
+// up to 6 digits are possible, as the largest possible 7 digit number score is 7*(9^5) = 413343, which is
+// only 6 digits long.
+func main() {
+	narcissists := []int{}
+
+	for i := 10; i <= 354294; i++ {
+		sum := 0
+
+		for _, digit := range digits(i) {
+			sum += numberutil.Pow(digit, 5)
+		}
+
+		if sum == i {
+			narcissists = append(narcissists, i)
+		}
+	}
+
+	fmt.Println("Narcissists: ", narcissists)
+	fmt.Println("Sum: ", numberutil.Sum(narcissists))
+}
+
+func digits(number int) (digits []int) {
+	for _, digitString := range strings.Split(strconv.Itoa(number), "") {
+		digit, _ := strconv.Atoi(digitString)
+		digits = append(digits, digit)
+	}
+
+	return digits
+}
